c296baafc13b2c344f4c0594b6711fa1613e1a20
Kunal1358 <kunalkohli8054@.com>
2021-10-09 15:52:11 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express=require('express');
+const port=8000;
+const app=express();
+
+
+app.listen(port,function(err){
+    if(err){
+        console.log(`Error running server: ${err}`);
+        return;
+    }
+    console.log("Server is up and running on port:",port);
+})
