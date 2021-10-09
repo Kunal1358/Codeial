d1278c531927bbcbb6f86d99b1a84e031a21ecf6
Kunal1358 <kunalkohli8054@.com>
2021-10-09 16:57:46 +0530

    Added view engine

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,8 @@
 module.exports.home = function(req,res){
 
-    return res.end('<h1> Home Controller </h1>');
+    return res.render('home',{
+        title: "Home"
+    });
 
 };
 
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,6 +1,8 @@
 module.exports.profile = function(req,res){
 
-    return res.end('<h1> Users Profile </h1>');
+    return res.render('users',{
+        title: "Home"
+    });
 
 };
 
