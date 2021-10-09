825f30d2c9c7a48eae9cbe816ec850c78fa0e9f0
Kunal1358 <kunalkohli8054@.com>
2021-10-09 16:05:37 +0530

    Added router

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express=require('express');
 const port=8000;
 const app=express();
 
+// Using express router
+app.use('/',require('./routes/index'));
 
 app.listen(port,function(err){
     if(err){
---
+++ routes/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const router = express.Router();
+
+
+console.log("Express Router")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports=router;
