bea50e7fae59f0c5488be72d80dc196b79d39b0f
Kunal1358 <kunalkohli8054@.com>
2021-10-09 17:39:02 +0530

    Added signIn and SignUp

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,8 +1,36 @@
 module.exports.profile = function(req,res){
 
-    return res.render('users',{
-        title: "Home"
+    return res.render('users_profile',{
+        title: "users_profile"
     });
+
+};
+
+// render sign in page
+module.exports.signUp = function(req,res){
+
+    return res.render('user_sign_up',{
+        title: "Codial : sign Up"
+    });
+};
+
+// render sign out page
+module.exports.signIn = function(req,res){
+
+    return res.render('user_sign_in',{
+        title: "Codial : sign In"
+    });
+};
+
+// get sign up data
+module.exports.create = function(req,res){
+    //TODO later
+
+};
+
+// Create sign in session
+module.exports.createSession = function(req,res){
+    //TODO later
 
 };
 
--- routes/users.js
+++ routes/users.js
@@ -6,8 +6,10 @@ const userController = require('../controllers/users_controller');
 
 router.get('/profile',userController.profile);
 
+/// sign in & up route
+router.get('/sign-up',userController.signUp);
 
-
+router.get('/sign-in',userController.signIn);
 
 
 
