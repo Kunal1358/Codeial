ab03a37522f1f496b3e11bfe3b172378a2648f9f
Kunal1358 <kunalkohli8054@.com>
2021-10-10 20:49:25 +0530

    Fetched post from DB and showed in home page

--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
 const postController = require('../controllers/posts_controller');
 
 // router.post('/create',postController.create);
-router.post('/create',postController.create);
+router.post('/create',passport.checkAuthentication,postController.create);
 
 
 
