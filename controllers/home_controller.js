c25405ea55cc72d9e364e3b30843282f4185980e
Kunal1358 <kunalkohli8054@.com>
2021-10-11 07:46:35 +0530

    Displayed comments on homepage

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -24,7 +24,15 @@ module.exports.home = function(req,res){
     // });
 
 
-    Post.find({}).populate('user').exec(function(err,posts){
+    Post.find({})
+    .populate('user')
+    .populate({
+        path: 'comments',
+        populate: {
+            path: 'user'
+        }
+    })
+    .exec(function(err,posts){
 
         if(err){console.error("Error Fetchinng the Posts from DB \n", err); return;}
 
