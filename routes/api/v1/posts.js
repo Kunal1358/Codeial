4a9562b9b9180562f023e20bdb442d6404159e5a
Kunal1358 <kunalkohli8054@.com>
2021-10-13 18:59:09 +0530

    Used Api for fetching and deleting posts

---
+++ routes/api/v1/posts.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+const postApi = require('../../../controllers/api/v1/post_api');
+
+router.get('/',postApi.index);
+router.delete('/:id',postApi.destroy);
+
+
+module.exports = router;
