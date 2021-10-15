167171c9d1c61fb0821d951fb39c4e1014153ffc
Kunal1358 <kunalkohli8054@.com>
2021-10-15 08:36:04 +0530

    Added Like model

---
+++ models/like.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const likeSchema = mongoose.Schema({
+
+    user:{
+        type: mongoose.Schema.Types.ObjectId
+    },
+    // this defines the object id of the liked object
+    likeable:{
+        type: mongoose.Schema.Types.ObjectId,
+        require: true,
+        refPath: 'onModel'
+    },
+    // this field is used for defining the type of the liked object since this is a dynamic reference
+    onModel:{
+        type: String,
+        required: true,
+        enum: ['Post', 'Comment']
+    }
+
+},{
+    timestamps: true
+});
+
+
+const Like = mongoose.model('Like', likeSchema);
+module.exports = Like;
