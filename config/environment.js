9bbb46d7db3e20a196b2953a5743d9c27fa82738
Kunal1358 <kunalkohli8054@.com>
2021-10-16 16:39:21 +0530

    Production Code Done

--- config/environment.js
+++ config/environment.js
@@ -1,11 +1,11 @@
 
-console.log('===================Inside env==================')
-
 const development = {
+
     name: 'development',
     // asset_path: '/assets',
     asset_path: './assets',
     session_cookie_key: 'blahsomething',
+    db: 'codeial_development',
     smtp: {
         service: 'gmail',
         host: 'smtp.gmail.com',
@@ -20,12 +20,34 @@ const development = {
     google_client_secret: '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',
     google_call_back_url: "http://localhost:8000/users/auth/google/callback",
     jwt_secret: 'codeial'
+
 }
 
 const production = {
-    name: 'production'
+
+    name: 'production',
+    asset_path: process.env.CODEIAL_ASSET_PATH_A,
+    session_cookie_key: process.env.CODEIAL_SESSION_COOKIE_KEY,
+    db: process.env.CODEIAL_DB,
+    smtp: {
+        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
+        auth: {
+            user: process.env.CODEIAL_GMAIL_USERNAME,
+            pass: process.env.CODEIAL_GMAIL_PASSWORD
+        }
+    },
+    google_client_id: process.env.CODEIAL_GOOGLE_CLIENT_ID,
+    google_client_secret: process.env.CODEIAL_GOOGLE_CLIENT_SECRET,
+    google_call_back_url: process.env.CODEIAL_GOOGLE_CALLBACK_URL, 
+    jwt_secret: process.env.CODEIAL_JWT_SECRET,
+
+    
 }
 
 
 
-module.exports = development;
+module.exports = eval(process.env.CODEIAL_ENVIRONMENT) == undefined ? development : eval(process.env.CODEIAL_ENVIRONMENT);
+
--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
+const env = require('./environment');
+mongoose.connect(`mongodb://localhost/${env.db}`);
 
-mongoose.connect('mongodb://localhost/codeial_development');
+// mongoose.connect('mongodb://localhost/codeial_development');
 
 const db = mongoose.connection;
 
