08b174f18bccc6a1db2bdfb4633f52d62487c1e5
Kunal1358 <kunalkohli8054@.com>
2021-10-16 11:37:06 +0530

    Created Chat Rooms

--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -9,6 +9,15 @@ module.exports.chatSockets = function(socketServer){
             console.log('socket disconnected!');
         });
 
+        socket.on('join_room', function(data){
+            console.log('joining request rec.', data);
+
+            socket.join(data.chatroom);
+
+            io.in(data.chatroom).emit('user_joined', data);
+        })
+
+
     });
 
 }
